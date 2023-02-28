1a6428628848260f74bfbf8909834192b81c4019
Dexter Dick <@dexterdick@hotmail.com>
2023-02-28 13:06:54 -0500

    updated to save to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,42 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+    openDB("jate", 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains("jate")) {
+                console.log("jate database already exists");
+                return;
+            }
+            db.createObjectStore("jate", {
+                keyPath: "id",
+                autoIncrement: true,
+            });
+            console.log("jate database created");
+        },
+    });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    console.log("Add data to database");
+    // Create a connection to the database database and version we want to use.
+    const contactDb = await openDB("jate", 1);
+
+    // Create a new transaction and specify the database and data privileges.
+    const tx = contactDb.transaction("jate", "readwrite");
+
+    // Open up the desired object store.
+    const store = tx.objectStore("jate");
+
+    // Use the .put() method to update all data in the database.
+    const request = store.put({ id: 1, value: content });
+
+    // Get confirmation of the request.
+    const result = await request;
+    console.log("result.value", result);
+    return result?.value;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
